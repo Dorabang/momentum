e269a379ba831e8682b24d14efd06fab45102058
Dorabang <psa1195@naver.com>
2023-02-23 20:24:41 +0900

    chore: Create React App install

---
+++ momentum/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'component/Background';
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h2>Hello</h2>
+      </Container>
+    </>
+  );
+}
+
+export default App;
