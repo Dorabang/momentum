eea884aea834779d1a085ba940c49b9e680f52d5
Dorabang <psa1195@naver.com>
2023-02-25 20:50:18 +0900

    design: Add Todo list css

--- momentum/src/style/style.js
+++ momentum/src/style/style.js
@@ -123,3 +123,70 @@ export const AutherStyle = styled.p`
   font-size: 1.4rem;
   padding-top: 10px;
 `;
+
+export const TodoForm = styled.form`
+  margin-top: 40px;
+`;
+
+export const TodoInput = styled.input`
+  display: inline-block;
+  width: 420px;
+  padding: 5px 0;
+  text-align: center;
+  font-family: "Noto Serif KR", serif;
+  border-bottom: 1px solid #222;
+  background: transparent;
+  font-size: 1.6rem;
+
+  @media ${(props) => props.theme.mobile} {
+    width: 60%;
+    text-align: center;
+  }
+`;
+
+export const List = styled.ul`
+  padding: 20px 0;
+  margin: 0 auto;
+  width: 500px;
+  text-align: left;
+
+  @media ${(props) => props.theme.tablet} {
+    width: 65%;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    width: 72%;
+  }
+
+  @media (max-width: 420px) {
+    width: 80%;
+  }
+
+  @media (max-width: 390px) {
+    width: 82%;
+  }
+`;
+
+export const ListObj = styled.li`
+  padding: 3px 0;
+`;
+
+export const SubmitBtn = styled.button`
+  display: inline-block;
+  padding: 5px 20px;
+  background: #222;
+  color: #fff;
+  font-family: "Noto Serif KR", serif;
+  border: 1px solid #222;
+  text-align: center;
+  margin-left: 10px;
+  font-size: 1.6rem;
+`;
+
+export const Btn = styled.button`
+  background: transparent;
+  font-family: "Noto Serif KR", serif;
+  border: 0;
+  margin-right: 10px;
+  font-size: 1.4rem;
+`;
