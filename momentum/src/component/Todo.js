be23d247c6a8ce3582332918a5d3914bd470edcb
Dorabang <psa1195@naver.com>
2023-02-25 20:50:02 +0900

    feat: Add Todo list method

--- momentum/src/component/Todo.js
+++ momentum/src/component/Todo.js
@@ -1,7 +1,52 @@
-const Todo = () => {
-  <>
-    <p>Todo</p>
-  </>;
+import React, { useState } from "react";
+import {
+  List,
+  TodoForm,
+  TodoInput,
+  SubmitBtn,
+  ListObj,
+  Btn,
+} from "style/style";
+
+const TodoList = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [...currentArray, todo]);
+    setTodo("");
+  };
+  const deleteBtn = (index) => {
+    setTodos((curTodos) =>
+      curTodos.filter((_, todoIndex) => index !== todoIndex)
+    );
+  };
+
+  return (
+    <>
+      <TodoForm onSubmit={onSubmit}>
+        <TodoInput
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        ></TodoInput>
+        <SubmitBtn>Add</SubmitBtn>
+      </TodoForm>
+      <List>
+        {todos.map((item, index) => (
+          <ListObj key={index}>
+            <Btn onClick={() => deleteBtn(index)}>✖</Btn>
+            {item}
+          </ListObj>
+        ))}
+      </List>
+    </>
+  );
 };
 
-export default Todo;
+export default TodoList;
