2e6e3ed1a0211d12abcd15896d35544a31495f95
Dorabang <psa1195@naver.com>
2023-02-25 19:44:36 +0900

    feat: Using API to Print Weather Information

--- momentum/src/component/Weather.js
+++ momentum/src/component/Weather.js
@@ -1,7 +1,34 @@
+import React, { useState } from "react";
+
 const Weather = () => {
-  <>
-    <p>Weather</p>
-  </>;
+  const API_KEY = "f2b7d9c737cfb0addca181e46a704453";
+
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const onGeoOk = (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCity(data.name);
+        setWeather(`${data.weather[0].main} / ${data.main.temp}℃`);
+      });
+  };
+
+  function onGeoError() {
+    alert("Can't find you. No weather for you.");
+  }
+
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+  return (
+    <>
+      <p>{city}</p>
+      <p>{weather}</p>
+    </>
+  );
 };
 
 export default Weather;
